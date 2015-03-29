ce33be51e11c89b686665ff119935439cb6d3194
James R Glasgow <james@jrglasgow.com>
2015-03-28 20:09:09 -0600

    adding capture_image.py which will capture an image from the camera and place it on a usb drive

---
+++ capture_image.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import gphoto2 as gp
+import os
+import time
+
+def capture_image(camera, context, target_dir='/tmp', sequence=0):
+  file_path = camera.capture(gp.GP_CAPTURE_IMAGE, context)
+  extention = file_path.name.split('.')[-1]
+  if (sequence != 0):
+    file_name = 'image-%05d.%s' % (sequence, extention)
+  else :
+    file_name = 'image-%s.%s' % (int(time.time()), extention)
+
+  target = os.path.join(target_dir, file_name)
+
+  camera_file = gp.check_result(camera.file_get(file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL, context))
+  gp.check_result(gp.gp_file_save(camera_file, target))
+
+
+if __name__=='__main__':
+  context = gp.Context()
+  camera = gp.Camera()
+  camera.init(context)
+  capture_image(camera, context, '/media/usb')
+  #capture_image(camera, context)
+  camera.exit(context)
+
